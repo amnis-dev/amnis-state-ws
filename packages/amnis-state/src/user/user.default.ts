d5fdaead9b8fafebe1185e4f3c44a930ba358e19
Eric Crowell <eric.crowell@ecrow.dev>
2022-06-09 19:53:57 +0200

    dev: Fixed build

--- packages/amnis-state/src/user/user.default.ts
+++ packages/amnis-state/src/user/user.default.ts
@@ -4,6 +4,7 @@ import { userKey } from './user';
 export const userDefault: User = entityCreate<User>(userKey, {
   name: 'eCrow',
   email: 'eric.crowell@ecrow.dev',
+  password: null,
   $roles: [],
   $permits: [],
 });
