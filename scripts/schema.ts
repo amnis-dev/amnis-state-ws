8efa00380834e9d290901f43bdae529fbec3cf72
Eric Crowell <eric.crowell@ecrow.dev>
2022-06-25 09:53:18 +0200

    dev: Moved schema files to amnis-core package

--- scripts/schema.ts
+++ scripts/schema.ts
@@ -20,19 +20,23 @@ typeSchemaFiles.forEach((filePath) => {
   fse.writeJSONSync(`${dir}/${prefix}.schema.json`, schema, { spaces: 2 });
 });
 
-const stateSchemaFiles = glob.sync('./packages/amnis-state/**/*.types.ts', {
+const coreSchemaFiles = glob.sync('./packages/amnis-core/src/types/*.types.ts', {
   nodir: true,
   ignore: './**/node_modules/**',
 });
 
-stateSchemaFiles.forEach((filePath) => {
-  const dir = path.dirname(filePath);
+coreSchemaFiles.every((filePath) => {
+  // const dir = path.dirname(filePath);
   const prefix = path.basename(filePath).split('.')[0];
+  if (prefix === 'database') {
+    return true;
+  }
   const schema = createGenerator({
-    schemaId: `state-${prefix}`,
+    schemaId: `core-${prefix}`,
     path: filePath,
     tsconfig: 'tsconfig.json',
     type: prefix.charAt(0).toUpperCase() + prefix.slice(1),
   }).createSchema('*');
-  fse.writeJSONSync(`${dir}/${prefix}.schema.json`, schema, { spaces: 2 });
+  fse.writeJSONSync(`./packages/amnis-core/src/schemas/${prefix}.schema.json`, schema, { spaces: 2 });
+  return true;
 });
