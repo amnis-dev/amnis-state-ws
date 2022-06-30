cda667fd663af7cf60858ab06a486ff19e2ca320
Eric Crowell <eric.crowell@ecrow.dev>
2022-06-30 15:36:21 +0200

    dev: Simplified imports to prevent node/browser environment cross error

--- packages/amnis-api/src/auth/index.ts
+++ packages/amnis-api/src/auth/index.ts
@@ -1,4 +1,2 @@
 export * from './auth.types';
-export * from './auth.process';
-export * from './auth.queries';
 export * from './auth.node';
--- packages/amnis-api/src/crud/index.ts
+++ packages/amnis-api/src/crud/index.ts
@@ -1,4 +1,2 @@
 export * from './crud.types';
-export * from './crud.process';
-export * from './crud.queries';
 export * from './crud.node';
