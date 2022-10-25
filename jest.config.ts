b34d6b545d2c0ceb13454ca3c39b73445432e58b
Eric Crowell <eric.crowell@ecrow.dev>
2022-10-25 19:03:45 +0200

    fix: Fixed broken jest tests when importing redux-toolkit ESM modules

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,9 @@ import type { Config } from 'jest';
 const jestConfig: Config = {
   verbose: true,
   testEnvironment: 'node',
+  // transform: {
+  //   '^.+\\.[t|j]sx?$': 'jest-esm-transformer',
+  // },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
     // '^@amnis/core/(.*)$': '<rootDir>/packages/amnis-core/src/$1',
