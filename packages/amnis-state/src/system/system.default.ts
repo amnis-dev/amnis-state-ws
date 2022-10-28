1fe5ca1cd1ef733843ff3ded77a1b13980a51b0c
Eric Crowell <eric.crowell@ecrow.dev>
2022-10-28 16:15:07 +0200

    fix: Removed outdated data model for initial database setup

--- packages/amnis-state/src/system/system.default.ts
+++ packages/amnis-state/src/system/system.default.ts
@@ -3,6 +3,7 @@ import { roleKey, systemCreate, uid } from '@amnis/core';
 export const systemDefault = systemCreate({
   name: 'Amnis System',
   $adminRole: uid(roleKey),
+  $execRole: uid(roleKey),
 });
 
 export default systemDefault;
