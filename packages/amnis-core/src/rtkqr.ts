3740fae8e3a4c49ffe1706c36201ab35fb7e8ec1
Eric Crowell <eric.crowell@ecrow.dev>
2022-11-09 16:42:42 +0100

    fix: Fixed type issue with react api query hooks

--- packages/amnis-core/src/rtkqr.ts
+++ packages/amnis-core/src/rtkqr.ts
@@ -1,13 +1,25 @@
 /* eslint-disable import/extensions */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /** @ts-ignore */
+import * as rtkqRaw from '@reduxjs/toolkit/dist/query/rtk-query.esm.js';
+/** @ts-ignore */
 import * as rtkqrRaw from '@reduxjs/toolkit/dist/query/react/rtk-query-react.esm.js';
+import type { CreateApi } from '@reduxjs/toolkit/dist/query/createApi.js';
+import type { reactHooksModuleName } from '@reduxjs/toolkit/dist/query/react/module.js';
 
-const rtkqr = rtkqrRaw as typeof import('@reduxjs/toolkit/dist/query/index.d.js');
+const rtkq = rtkqRaw as {
+  fetchBaseQuery: typeof import('@reduxjs/toolkit/dist/query/index.d.js').fetchBaseQuery
+};
+const rtkqr = rtkqrRaw as {
+  createApi: CreateApi<typeof import('@reduxjs/toolkit/dist/query/core/module.js').coreModuleName | typeof reactHooksModuleName>
+};
+
+export const {
+  fetchBaseQuery,
+} = rtkq;
 
 export const {
   createApi,
-  fetchBaseQuery,
 } = rtkqr;
 
 export default rtkqr;
