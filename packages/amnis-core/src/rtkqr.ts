0b7ff9b660d57abe7efd0a6e18d99f1054366244
Eric Crowell <eric.crowell@ecrow.dev>
2022-11-11 09:01:08 +0100

    fix: Fixed typing issues with react api

--- packages/amnis-core/src/rtkqr.ts
+++ packages/amnis-core/src/rtkqr.ts
@@ -1,25 +1,19 @@
-/* eslint-disable import/extensions */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-/** @ts-ignore */
-import * as rtkqRaw from '@reduxjs/toolkit/dist/query/rtk-query.esm.js';
-/** @ts-ignore */
-import * as rtkqrRaw from '@reduxjs/toolkit/dist/query/react/rtk-query-react.esm.js';
-import type { CreateApi } from '@reduxjs/toolkit/dist/query/createApi.js';
-import type { reactHooksModuleName } from '@reduxjs/toolkit/dist/query/react/module.js';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as rtkqRaw from '@reduxjs/toolkit/dist/query/index.js';
+import * as rtkqReactRaw from '@reduxjs/toolkit/dist/query/react/index.js';
 
-const rtkq = rtkqRaw as {
-  fetchBaseQuery: typeof import('@reduxjs/toolkit/dist/query/index.d.js').fetchBaseQuery
-};
-const rtkqr = rtkqrRaw as {
-  createApi: CreateApi<typeof import('@reduxjs/toolkit/dist/query/core/module.js').coreModuleName | typeof reactHooksModuleName>
-};
+const rtkq = (
+  (rtkqRaw as any).default ?? rtkqRaw
+) as typeof rtkqRaw;
+const { reactHooksModule } = (
+  (rtkqReactRaw as any).default ?? rtkqReactRaw
+) as typeof rtkqReactRaw;
 
-export const {
-  fetchBaseQuery,
-} = rtkq;
+export const createApi = rtkq.buildCreateApi(
+  rtkq.coreModule(),
+  reactHooksModule(),
+);
 
-export const {
-  createApi,
-} = rtkqr;
+export const { fetchBaseQuery } = rtkq;
 
-export default rtkqr;
+export default { createApi, fetchBaseQuery };
