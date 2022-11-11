4c1f2991c3efc0616c410ee2d461e9f2df068c55
Eric Crowell <eric.crowell@ecrow.dev>
2022-11-11 18:12:29 +0100

    feat: Amnis API now exports utility methods

--- packages/amnis-api/src/index.react.ts
+++ packages/amnis-api/src/index.react.ts
@@ -2,7 +2,7 @@ export * from './types.js';
 export * from './config.js';
 export * from './reducers.js';
 export * from './set.react.js';
-
+export * from './util/index.js';
 export * from './spa/index.js';
 export * from './auth/index.react.js';
 export * from './crud/index.react.js';
--- packages/amnis-api/src/index.ts
+++ packages/amnis-api/src/index.ts
@@ -2,6 +2,7 @@ export * from './types.js';
 export * from './config.js';
 export * from './reducers.js';
 export * from './set.js';
+export * from './util/index.js';
 export * from './spa/index.js';
 export * from './auth/index.js';
 export * from './crud/index.js';
