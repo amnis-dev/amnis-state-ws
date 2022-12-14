d38f7b7394c550d56979437b6be1fac187ea88ff
Eric Crowell <eric.crowell@ecrow.dev>
2022-12-14 13:14:19 +0100

    feat: Added success logs for delete operations

--- packages/amnis-process/src/crud/crud.delete.ts
+++ packages/amnis-process/src/crud/crud.delete.ts
@@ -70,6 +70,18 @@ Io<StateDeleter, StateDeleter>
     }
 
     /**
+     * Create a possible success output.
+     */
+    const acceptedKeys = Object.keys(result).filter((key) => !deniedKeys.includes(key));
+    if (acceptedKeys.length > 0) {
+      output.json.logs.push({
+        level: 'success',
+        title: 'Deletion Successful',
+        description: `Successfully deleted records in collection${acceptedKeys.length > 1 ? 's' : ''}: ${acceptedKeys.join(', ')}.`,
+      });
+    }
+
+    /**
      * Create historic records of the delete.
      * TODO: Determine if necessary.
      */
